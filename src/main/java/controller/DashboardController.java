1232136bc347a4009c3d6ffbfdb2778d67e28482
SarangaKalhari <kalharisaranga9@gmail.com>
2025-10-11 23:52:40 +0530

    Add basic dashbord

---
+++ src/main/java/controller/DashboardController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DashboardController {
+
+    @FXML
+    void customerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void employeeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void itemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void supplierOnAction(ActionEvent event) {
+
+    }
+
+}
